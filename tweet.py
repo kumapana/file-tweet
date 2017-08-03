8c2593d94a1cbd43fa17d8cfa1408d3c8e74707a
panakuma <kumachan05-11@hotmail.co.jp>
2017-08-03 20:21:48 +0900

    レスポンスを分かりやすくした

--- tweet.py
+++ tweet.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-
-from requests_oauthlib import OAuth1Session
-import time
-
-from datetime import datetime
-datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-
-tweeted_text="initialize"
-new_tweet_text="initialize"
 
 #アクセストークン
 CK='<customer key>'
@@ -18,8 +8,20 @@ AS='<access secret>'
 #読み込むテキストファイル
 file_name='<読み込み先>'
 
+#ファイルを読みに行く間隔
+sleep_time=<秒単位で指定>
+
 #投稿先URL
 url = "https://api.twitter.com/1.1/statuses/update.json"
+
+#ここより下は分かる人のみ弄って下さい
+#--------------------------------------------------------------------------------------------------
+from requests_oauthlib import OAuth1Session
+import time
+
+tweeted_text="initialize"
+new_tweet_text="initialize"
+
 
 def tweet(text):
   #ツイート内容
@@ -29,31 +31,29 @@ def tweet(text):
   twitter = OAuth1Session(CK, CS, AT, AS)
   req = twitter.post(url,params=params)
   
-  #ツイート内容をtweeted_textに保存
-  tweeted_text=text
-  
   #ツイート結果表示
   if req.status_code==200:
-    print("OK")
+    print("以下の内容をツイートしました : ",text)
   else:
-    print("Error: %d"%req.status_code)
+    print("Error: %d  以下の内容をツイートできませんでした : "%req.status_code,text)
 
 
 num=0
 while(num==0):
  #テキストファイル読み込み
-  load_file=open(file_name,'r')
+  load_file=open(file_name,'r',encoding="utf-8")
   new_tweet_text=load_file.read()
 
-  #前回読み込んだファイルと付きあわせて、変更されていればツイートする
+  #前回読み込んだファイルと付きあわせて、変更されていればツイートして、ツイート内容をtweeted_textに保存する
   if new_tweet_text!=tweeted_text:
+    tweeted_text=new_tweet_text
     if len.(new_tweet_text.encode("UTF-8"))<=140:
       tweet(new_tweet_text)
       load_file.close()
     else:
       load_file.close()
       print("140文字を超えているため、ツイートしませんでした。")
-      return 1
   else:
     load_file.close()
-  time.sleep(30) 
+    print("ファイルの内容が前回ツイートと同じだったので、ツイートしませんでした。")
+  time.sleep(sleep_time) 
